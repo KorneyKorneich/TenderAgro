979b0e6ab4f3f742b4a54c0b6bec124cff2b0eda
Nikita <joyyolok@gmail.com>
2024-09-23 16:23:38 +0300

    gh-pages set up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 py-6 text-center text-white">
-      <p>© 2024 Ваша компания</p>
+      <p>© 2024 TENDER Agro</p>
     </footer>
   );
 };
 
 export default Footer;
+
