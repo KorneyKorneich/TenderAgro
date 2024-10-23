b297307eab6d7a54b85073c1c3d105c8c05a9f5e
Nikita <joyyolok@gmail.com>
2024-10-23 23:34:15 +0300

    change some texts, fix the background image

--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -11,20 +11,22 @@ interface InfoSectionProps {
 const InfoSection = ({
   h1Text,
   pText,
+  img, // используем для фоновой картинки
   buttonText,
   buttonHandler,
 }: InfoSectionProps) => {
   return (
     <>
-      <div className="relative mb-7 bg-white py-10 text-center bg-flakes-in-bowl bg-no-repeat">
+      <div
+        className="relative mb-7 text-center bg-no-repeat bg-cover bg-center min-h-[400px] lg:min-h-[500px] xl:min-h-[600px] flex items-center justify-center"
+        style={{ backgroundImage: `url(${img})` }} // устанавливаем фоновое изображение
+      >
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative z-10">
-          <h1 className="w-3/4 mx-auto text-4xl font-bold text-white">
-            {h1Text}
-          </h1>
-          <p className="w-3/4 mt-2 mx-auto text-xl text-white">{pText}</p>
+        <div className="relative z-10 w-3/4 mx-auto text-white flex flex-col items-center">
+          <h1 className="text-4xl font-bold">{h1Text}</h1>
+          <p className="mt-2 text-xl">{pText}</p>
           <Button
-            className="mt-2"
+            className="mt-4"
             buttonHandler={buttonHandler}
             buttonText={buttonText}
           />
